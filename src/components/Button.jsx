e5475e34c18dfdfd3d0f407c329c97f39a6bf8c2
Avétis KAZARIAN <avetis@kazarian.fr>
2024-01-25 13:42:16 +0100

    feat: delete in todo list

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,21 @@
 import clsx from "clsx"
-import { twMerge } from "tailwind-merge"
 
-export const Button = ({ className, ...otherProps }) => (
+const variants = {
+  primary: "bg-indigo-600 active:bg-indigo-700 text-white",
+  danger: "bg-red-500 active:bg-red-600 text-white",
+}
+const sizes = {
+  md: "text-sm px-2 py-1",
+  lg: "text-lg font-semibold px-4 py-3",
+}
+export const Button = ({
+  className,
+  variant = "primary",
+  size = "lg",
+  ...otherProps
+}) => (
   <button
-    className={twMerge(
-      clsx(
-        "bg-indigo-600 active:bg-indigo-700 text-white text-lg font-semibold px-4 py-3",
-        className,
-      ),
-    )}
+    className={clsx(variants[variant], sizes[size], className)}
     {...otherProps}
   />
 )
--- src/pages/todos.jsx
+++ src/pages/todos.jsx
@@ -1,29 +1,55 @@
+import { Button } from "@/components/Button"
 import { readDatabase } from "@/db/readDatabase"
+import axios from "axios"
 import clsx from "clsx"
 import Link from "next/link"
+import { useState } from "react"
 
 export const getServerSideProps = async () => {
   const { todos } = await readDatabase()
 
   return {
-    props: { todos: Object.values(todos) },
+    props: { initialTodos: Object.values(todos) },
   }
 }
-const TodosPage = ({ todos }) => (
-  <ul className="flex flex-col gap-4">
-    {todos.map(({ id, description, isDone }) => (
-      <li key={id}>
-        <Link href={`/todos/${id}/edit`} className="flex gap-2">
-          <span
-            className={clsx("w-6 h-6 border border-green-500", {
-              "bg-green-500": isDone,
-            })}
-          />{" "}
-          #{id} {description}
-        </Link>
-      </li>
-    ))}
-  </ul>
-)
+const TodosPage = ({ initialTodos }) => {
+  const [todos, setTodos] = useState(initialTodos)
+  const handleDelete = (todoId) => async () => {
+    const deletedTodo = todos.find(({ id }) => id === todoId)
+    const newTodos = todos.filter(({ id }) => id !== todoId)
+    setTodos(newTodos)
+
+    try {
+      await axios.delete(`http://localhost:3000/api/todos/${todoId}`)
+    } catch (err) {
+      setTodos([...newTodos, deletedTodo])
+    }
+  }
+
+  return (
+    <ul className="flex flex-col gap-4">
+      {todos.map(({ id, description, isDone }) => (
+        <li key={id} className="flex items-center gap-2 group">
+          <Link href={`/todos/${id}/edit`} className="flex gap-2 py-1">
+            <span
+              className={clsx("w-6 h-6 border border-green-500", {
+                "bg-green-500": isDone,
+              })}
+            />{" "}
+            #{id} {description}
+          </Link>
+          <Button
+            onClick={handleDelete(id)}
+            variant="danger"
+            size="md"
+            className="group-hover:inline hidden"
+          >
+            DELETE
+          </Button>
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 export default TodosPage
